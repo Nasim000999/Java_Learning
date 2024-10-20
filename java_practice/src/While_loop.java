7238fb051edade7bb3f4e8c8186038075da060e4
Nasim000999 <nasim33467@gmail.com>
2024-10-20 13:03:30 +0530

    Add files via upload

---
+++ java_practice/src/While_loop.java
@@ -0,0 +1,18 @@
+public class While_loop{
+    public static void main(String[] args) {
+        int i = 1;
+        while (1<100 {
+            System.out.println(1);
+            i++:
+
+        }
+    }
+}
+// ____ the above exaple is for the while loop exp
+
+//  Do while loop
+//============================================================================
+//public static void main(String[] args) {
+
+
+

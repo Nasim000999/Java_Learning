a5faf2dee0a282ca05972e5eedec328db5be2da7
Nasim000999 <nasim33467@gmail.com>
2024-10-20 13:08:26 +0530

    Add files via upload

---
+++ Char_Data_Type/Char_Data_type.java
@@ -0,0 +1,19 @@
+package Char_Data_Type;
+
+import java.util.Scanner;
+public class Char_Data_type {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = 'z';
+        System.out.println(ch);
+        int x = (int)ch;
+        System.out.println(x);
+        // below are all charachters
+        //ABCDEFGHIJKLMNOPQRSTUVWXYZ
+        //0123456789
+        //!@#$%^&*()
+        // the value of a = 97
+        // the value of A = 65
+
+    }
+}
---
+++ increament_decreament.java
@@ -0,0 +1,27 @@
+package BasicsOfJava.basics;
+
+public class increament_decreament {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+    }
+//        int x =5;
+//        System.out.println(x);
+//        x = x+4;
+//        System.out.println(x);
+//        x++;
+//        System.out.println(x);
+//        x++;//x++ ka matlab h x+1
+//        /*System.out.println(x);
+//        x--;//x-- ka matlab x-1
+//        System.out.println(x);
+////this is single line comment*/
+////        int n = 14;
+////        int s = 19;
+////        System.out.println(n+s);
+////    } shortcut for multiple line comment ctrl +/
+//        System.out.println(x);
+
+}
